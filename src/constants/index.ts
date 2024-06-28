e9845baa1de8a11813f0ad06d9e7b0130147f71f
covelitein <abrahamcovenant2004@gmail.com>
2024-06-28 20:34:13 +0100

    project update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://javat365-assessment.vercel.app/api/",
 });
 
 export default api;
