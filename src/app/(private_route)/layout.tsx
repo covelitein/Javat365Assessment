048e07a50387b9a15a861213b84126827860065c
covelitein <abrahamcovenant2004@gmail.com>
2024-06-28 20:31:38 +0100

    project update

--- src/app/(private_route)/layout.tsx
+++ src/app/(private_route)/layout.tsx
@@ -2,11 +2,10 @@ import React from "react"
 import { getServerSession } from "next-auth"
 import authOptions from "@/auth/auth";
 import { redirect } from "next/navigation";
-import { AuthProvider } from "@/auth";
 import { Props } from "@/types";
 
 export default async function PrivateLayout({children}: Props){
   const session = await getServerSession(authOptions);
    if(!session?.user) redirect("/auth/signin")
-   return <><AuthProvider>{children}</AuthProvider></>
+   return <>{children}</>
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "https://javat365-assessment.vercel.app/api/",
+  baseURL: "http://localhost:3000/api/",
 });
 
 export default api;
